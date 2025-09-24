afa8bb485f8488622208a12f0d11b2769637eb3b
MK-explorer <argneo0@gmail.com>
2025-09-24 22:17:44 +0300

    initial commit

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"FibonacciPrimes","l":"FibonacciPrimes()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"FibonacciPrimes","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
